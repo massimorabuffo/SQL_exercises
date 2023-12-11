9bf9b178b4c1a94458b624ddc7200ee2e606d8d9
Massimo <rabuffo.massimo@gmail.com>
2023-12-11 20:24:42 +0100

    exercise_4

--- SQLite.3.sql
+++ SQLite.3.sql
@@ -1,3 +1,7 @@
 INSERT INTO Books VALUES (1, 'A Journey to the Center', 'Jules Verne', 'Adventure', 1864, 1234567890123, 12.99, 4.5, 10, 350, 'Verne Publishing');
 INSERT INTO Books VALUES (2, 'War and Peace', 'Leo Tolstoy', 'Historical', 1869, 1234567890124, 14.99, 4.8, 5, 1200, 'Tolstoy Prints');
 INSERT INTO Books VALUES (3, 'Whispers of the Wind', 'Amelia Blackburn', 'Romance', 1982, 1234567890125, 9.99, 4.2, 20, 275, 'Blackburn House');
+
+-- Exercise 4
+
+UPDATE Books SET price = 10.99, stock_count = 4 WHERE book_id = 2
